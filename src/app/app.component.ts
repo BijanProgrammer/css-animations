cd7822a243339728aaf5dcf25b88b5ebad38b4b0
BijanProgrammer <bijaneisapour@gmail.com>
2022-05-06 08:32:51 +0430

    [UPDATE] disable animations on page load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+    public noop: boolean = true;
+
+    public ngOnInit(): void {
+        setTimeout(() => (this.noop = false), 1000);
+    }
+}
